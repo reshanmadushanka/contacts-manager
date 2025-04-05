a9e0f24c24f5a7ee4778be3fd7e09d3ac7fa88df
Reshan Wijerathna <46405486+reshanmadushanka@users.noreply.github.com>
2025-04-05 09:26:02 +0530

    feat(front): implement frontend using vue js
    - add axio boostrap pinia
    - setup folder structure with files

--- contact-list-frontend/src/main.js
+++ contact-list-frontend/src/main.js
@@ -2,6 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +14,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(axios)
 app.mount('#app')
